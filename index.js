2b6abed1df7dbfd53eb74500dcb1eada3efe8d28
yashesh07 <87408807+yashesh07@users.noreply.github.com>
2022-04-01 17:13:39 +0530

    strting files added

---
+++ index.js
@@ -0,0 +1,55 @@
+var numberOfButtons = document.querySelectorAll(".drum").length;
+
+for(var i = 0; i<numberOfButtons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+
+        var buttonPressed = this.innerHTML;
+        makeSound(buttonPressed);
+        buttonAnimation(buttonPressed);
+    });
+}
+
+document.addEventListener("keydown", function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+function makeSound(key){
+    switch(key) {
+        case "w":
+            new Audio('sounds/crash.mp3').play();
+            break;
+        case "a":
+            new Audio('sounds/kick-bass.mp3').play();
+            break;
+        case "s":
+            new Audio('sounds/snare.mp3').play();
+            break;
+        case "d":
+            new Audio('sounds/tom-1.mp3').play();
+            break;
+        case "j":
+            new Audio('sounds/tom-2.mp3').play();
+            break;
+        case "k":
+            new Audio('sounds/tom-3.mp3').play();
+            break;
+        case "l":
+            new Audio('sounds/tom-4.mp3').play();
+            break;
+        default:
+
+    }
+}
+
+function buttonAnimation(currentKey){
+
+    var activeButton = document.querySelector("." + currentKey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100);
+
+}
